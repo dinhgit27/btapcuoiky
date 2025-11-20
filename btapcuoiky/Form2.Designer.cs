ce0a7b49ed39d87ca03a19a8381ef34fa1f9ce8a
Nguyễn Đỉnh <dinhnguyen.27062005@gmail.com>
2025-11-20 20:07:37 +0700

    commit tumlumlai

--- btapcuoiky/Form2.Designer.cs
+++ btapcuoiky/Form2.Designer.cs
@@ -85,9 +85,9 @@
             this.label3.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(102, 103);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(28, 45);
+            this.label3.Size = new System.Drawing.Size(146, 45);
             this.label3.TabIndex = 20;
-            this.label3.Text = " ";
+            this.label3.Text = "SETTING";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // groupBox2
